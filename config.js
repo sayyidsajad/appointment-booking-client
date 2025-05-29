044422289fe94b3f885dc7c2699c332b00a3f1c6
Sayyid Sajad Ottakath <111043430+sayyidsajad@users.noreply.github.com>
2025-05-29 12:36:28 +0530

    Add config.js to frontend

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-  API_BASE_URL: "http://127.0.0.1:5000/api",
+  API_BASE_URL: "https://appointment-booking-api-53qk.onrender.com/api", // for production
+  // API_BASE_URL: "http://127.0.0.1:5000/api", -- for development environment
 };
